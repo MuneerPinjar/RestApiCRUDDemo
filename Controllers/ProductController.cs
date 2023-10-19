9060d8c065851925273dfedd88f06fbc4b4f449e
Muneer Pinjar <munna.ieee@gmail.com>
2023-10-19 10:32:40 +0530

    code changes

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using RestApiCRUDDemo.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiCRUDDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly BootCampDemoContext _context;
+
+        //Constructor Injection (DI)
+        public ProductController(BootCampDemoContext context)
+        {
+            _context = context;
+        }
+
+
+        //To list all the products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Products>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+
+        }
+
+        // Get: api/products/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Products>> GetProducts(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return products;
+
+        }
+
+        //Post : api/products
+        [HttpPost]
+        public async Task<ActionResult<Products>> PostProducts(Products products)
+        {
+            _context.Products.Add(products);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProducts", new { id = products.ProductId }, products);
+        }
+
+        //Put: api/products/id
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Products>> PutProducts(int id, Products products)
+        {
+            if(id != products.ProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(products).State = EntityState.Modified;
+
+
+            try
+            {
+              await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else { throw; }
+                
+            }
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Products>> DeleteProducts(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+
+        }
+            private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestApiCRUDDemo.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,10 @@ namespace RestApiCRUDDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Connection
+            var connection = Configuration.GetConnectionString("BootCampDbConn");
+            services.AddDbContextPool<BootCampDemoContext>(options => options.UseSqlServer(connection));
+
             services.AddControllers();
         }
 
